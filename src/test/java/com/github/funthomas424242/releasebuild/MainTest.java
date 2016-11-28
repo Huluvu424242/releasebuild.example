9ed132666d8fb342b32cef1067d360702d9011fd
FunThomas424242 <funthomas424242@gmail.com>
2016-11-28 23:14:49 +0100

    test added
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

---
+++ src/test/java/com/github/funthomas424242/releasebuild/MainTest.java
@@ -0,0 +1,15 @@
+package com.github.funthomas424242.releasebuild;
+
+import org.junit.Test;
+
+/**
+ * Created by huluvu424242 on 28.11.16.
+ */
+public class MainTest {
+
+    @Test
+    public void testPrint_ValidCall(){
+        Main.main(null);
+    }
+
+}
